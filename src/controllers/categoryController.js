6564d0ae195037bbfce1545791b015e72a38dd2a
developeralamgirbd <developer.alamgirbd@gmail.com>
2023-03-08 17:40:33 +0600

    category

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,184 +1,3 @@
-/*const CategoryModel = require('../models/category/Category');
-const ProductModel = require('../models/product/Product');
-const SubCategoryModel = require('../models/category/SubCategory');
-
-const mongoose = require('mongoose');
-const ObjectId = mongoose.Types.ObjectId;
-
-const {checkAssociateService} = require("../services/common/checkAssociateService");
-
-const {showCategoriesService,
-    categoryFindByName,
-    categoryFindByID,
-    categoryUpdateService,
-    categoryCreateService,
-    categoryDeleteService} = require("../services/categoryService/categoryService");
-const getByIdService = require("../services/common/getByIdService");*/
-
-/*
-exports.postCategory = async (req, res)=>{
-    try {
-        const {name} = req.body;
-        const userID = req.auth._id;
-
-       const findCategory = await getByIdService({name}, CategoryModel);
-
-       if (findCategory){
-          return res.status(400).json({
-               status: 'fail',
-               error: 'Category already created',
-           });
-       }
-
-        const category = await categoryCreateService(name, userID);
-        res.status(200).json({
-            status: 'success',
-            message: 'Successfully created category',
-            data: category
-        });
-
-    }catch (error) {
-        console.log(error);
-        res.status(500).json({
-            status: 'fail',
-            error: 'Server error occurred'
-        });
-    }
-
-};
-
-exports.showCategory = async (req, res)=>{
-    try {
-
-        const categories = await showCategoriesService();
-
-        // if (!categories[0]){
-        //    return res.status(400).json({
-        //         status: 'fail',
-        //         message: 'Category not found',
-        //     });
-        // }
-
-        res.status(200).json({
-            status: 'success',
-            data: categories
-        });
-    }catch (error) {
-        console.log(error);
-        res.status(500).json({
-            status: 'fail',
-            error: error.message
-        });
-    }
-}
-
-exports.getSingleCategory = async (req, res)=>{
-    try {
-        const catID = req.params.categoryID;
-
-        const category = await categoryFindByID(catID);
-
-        if (!category[0]){
-           return res.status(400).json({
-                status: 'fail',
-                message: 'Category not found',
-            });
-        }
-
-        res.status(200).json({
-            status: 'success',
-            message: 'Successfully get all category',
-            data: category[0]
-        });
-    }catch (error) {
-        console.log(error);
-        res.status(500).json({
-            status: 'fail',
-            error: 'Server error occurred'
-        });
-    }
-}
-
-exports.updateCategory = async (req, res)=>{
-    try {
-
-        const _id = req.params.categoryID;
-        const name = req.body.name;
-        const authorID = req.auth._id;
-
-        if (name === ''){
-            return res.status(400).json({
-                status: 'fail',
-                error: 'Category name is required'
-            });
-        }
-
-        const result = await categoryUpdateService(_id, authorID, name)
-
-        if (!result){
-            return res.status(400).json({
-                status: 'fail',
-                error: 'Category not update'
-            });
-        }
-
-        res.status(200).json({
-            status: 'success',
-            result
-        });
-
-    }catch (error) {
-        console.log(error)
-        res.status(500).json({
-            status: 'fail',
-            error: 'Server error occurred'
-        });
-    }
-}
-
-exports.deleteCategory = async (req, res)=>{
-    try {
-        const _id = req.params.categoryID;
-        const authorID = req.auth._id;
-
-        const ObjectId = mongoose.Types.ObjectId;
-
-        const isCategory = await categoryFindByID(_id);
-
-        if(!isCategory[0]){
-            return res.status(400).json({
-                status: 'fail',
-                error: 'Category not found',
-            });
-        }
-
-        const CheckAssociate = await checkAssociateService({categoryID: ObjectId(_id)}, PostModel);
-
-        if (CheckAssociate[0]){
-            return res.status(400).json({
-                status: 'fail',
-                error: 'Delete failed, Category associate with post'
-            });
-        }
-
-        const result = await categoryDeleteService(authorID, _id);
-
-        res.status(200).json({
-            status: 'success',
-            data: result
-        });
-
-
-    }catch (error) {
-        console.log(error);
-        res.status(500).json({
-            status: 'fail',
-            error: 'Server error occurred'
-        });
-    }
-}
-*/
-
 
 const CategoryModel = require('../models/category/Category');
 const ProductModel = require('../models/product/Product');
@@ -193,6 +12,7 @@ const getByPropertyService = require("../services/common/getByPropertyService");
 const createService = require("../services/common/createService");
 const {updateService} = require("../services/common/updateService");
 const {showCategoriesService} = require("../services/categoryService/categoryService");
+const Category = require("../models/category/Category");
 
 exports.postCategory= async (req, res)=>{
     try {
@@ -239,9 +59,43 @@ exports.getCategories = async (req, res)=>{
 
 exports.getCategoryByID = async (req, res)=>{
     try {
-        const id = req.params.categoryID;
+        const id = req.params.id;
+        const childName = req.params.childName.toLowerCase();
 
-        const category = await getByIdService(id, CategoryModel);
+        const category = await CategoryModel.aggregate([
+            {$lookup: {from: 'subcategories', localField: '_id', foreignField: 'parentID', as: 'subCategory'}},
+            {$match: {$or: [{_id: ObjectId(id)},{"subCategory._id": ObjectId(id)},{ 'subCategory.children': {$in: [childName] } }]}}
+        ]);
+
+        // if (!category){
+        //     return res.status(404).json({
+        //         message: 'Category not found',
+        //     });
+        // }
+
+        res.status(200).json({
+            category
+        });
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: 'Server error occurred'
+        });
+    }
+}
+
+
+exports.getCategoryByName = async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const name = req.params.name;
+
+        // const category = await getByIdService(id, CategoryModel);
+        // CategoryModel.aggregate([
+        //     {$match: {}}
+        // ])
+        // CategoryModel.findOne({})
+        CategoryModel.findOne( { $or: [ { name: name }, { price: 10 } ] } )
 
         if (!category){
             return res.status(404).json({
@@ -263,13 +117,20 @@ exports.getCategoryByID = async (req, res)=>{
 exports.patchCategory = async (req, res)=>{
     try {
 
-        const id = req.params?.categoryID;
-        const name = req.body?.name;
+        const id = req.params?.id;
         const authorID = req.auth?._id;
 
         const category = await getByIdService(id, CategoryModel);
 
         const categoryName = req.body?.name !== '' ? req.body?.name : category?.name;
+
+        const isMatch = await getByPropertyService({_id: {$ne: ObjectId(id)}, name: categoryName}, CategoryModel);
+
+        if (isMatch){
+            return res.status(400).json({
+                error: 'Category already exit',
+            });
+        }
 
         const result = await updateService({_id: ObjectId(id), authorID: ObjectId(authorID)}, {name: categoryName}, CategoryModel);
         res.status(200).json({
@@ -286,7 +147,7 @@ exports.patchCategory = async (req, res)=>{
 
 exports.deleteCategory = async (req, res)=>{
     try {
-        const id = req.params?.categoryID;
+        const id = req.params?.id;
         const CheckAssociate = await checkAssociateService({categoryID: ObjectId(id)}, ProductModel);
 
         if (CheckAssociate[0]){
@@ -320,8 +181,8 @@ exports.deleteCategory = async (req, res)=>{
         });
     }
 }
-// Sub Category Controller
 
+// Sub Category Controller
 exports.postSubCategory = async (req, res, next)=>{
     try {
         const {name, parentID} = req.body;
@@ -346,14 +207,42 @@ exports.postSubCategory = async (req, res, next)=>{
 
 };
 
+exports.patchSubCategory = async (req, res)=>{
+    try {
+
+        const id = req.params?.id;
+        const authorID = req.auth?._id;
+
+        const category = await getByIdService(id, SubCategoryModel);
+
+        const categoryName = req.body?.name !== '' ? req.body?.name : category?.name;
+
+        const isMatch = await getByPropertyService({_id: {$ne: ObjectId(id)}, name: categoryName.toLowerCase()}, SubCategoryModel);
+
+        if (isMatch) {
+            return res.status(400).json({error: 'Sub Category already exits'})
+        }
+
+        const result = await updateService({_id: ObjectId(id), authorID: ObjectId(authorID)}, {name: categoryName}, SubCategoryModel);
+        res.status(200).json({
+            result
+        });
+
+    }catch (error) {
+        console.log(error)
+        res.status(500).json({
+            error: 'Server error occurred'
+        });
+    }
+}
+
 exports.postSubCategoryChild = async (req, res, next)=>{
     try {
-        const {name, id} = req.body;
+        const {name, id, index} = req.body;
+        const key = `children.${index}`;
+        const update = index === undefined ? {$addToSet: {children: name}} : {$set: {[key]: name}};
 
-        const children = await SubCategoryModel.updateOne({_id: ObjectId(id)}, {
-            $addToSet: {children: name}
-        }, {upsert: true})
-
+        const children = await SubCategoryModel.updateOne({_id: ObjectId(id)}, update, {upsert: true})
         res.status(200).json({
             children
         });
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,20 +1,21 @@
 const router = require('express').Router();
 
 const {deleteCategory, postCategory, deleteSubChildCategory,
-    postSubCategory, postSubCategoryChild, getCategories, getCategoryByID, patchCategory
+    postSubCategory, postSubCategoryChild, getCategories, getCategoryByID, patchCategory, patchSubCategory
 } = require('../controllers/categoryController');
 const {AuthVerifyMiddleware} = require("../middleware/AuthVerifyMiddleware");
 
 
 router.post('/categories', AuthVerifyMiddleware, postCategory);
 router.get('/categories', getCategories);
-router.get('/categories/:categoryID', AuthVerifyMiddleware, getCategoryByID);
-router.patch('/categories/:categoryID', AuthVerifyMiddleware, patchCategory);
-router.delete('/categories/:categoryID', AuthVerifyMiddleware, deleteCategory);
+router.get('/categories/:id/:childName', AuthVerifyMiddleware, getCategoryByID);
+router.patch('/categories/:id', AuthVerifyMiddleware, patchCategory);
+router.delete('/categories/:id', AuthVerifyMiddleware, deleteCategory);
 
 // Sub Category Route
 router.post('/sub-categories', AuthVerifyMiddleware, postSubCategory);
-router.post('/sub-categories-children', AuthVerifyMiddleware, postSubCategoryChild);
-router.patch('/sub-categories-children', AuthVerifyMiddleware, deleteSubChildCategory);
+router.put('/sub-categories-children', AuthVerifyMiddleware, postSubCategoryChild);
+router.patch('/sub-categories/:id', AuthVerifyMiddleware, patchSubCategory);
+router.delete('/sub-categories-children', AuthVerifyMiddleware, deleteSubChildCategory);
 
 module.exports = router;
